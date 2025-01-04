cd59e93e3e3de03c63a85c61bc9427f8e54684ca
Borjan Valchanov <borjan-val@proton.me>
2025-01-04 19:42:54 +0000

    Configure test runs to use development environment variables compatible with built-in database container

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -1,3 +1,3 @@
 alias test-build="docker build -t ticketing-hsg /workspaces/ticketing-hsg"
-alias test-run="docker run -d -p 80:80 --name ticketing-hsg ticketing-hsg"
+alias test-run="docker run -d -p 80:80 --env-file /workspaces/ticketing-hsg/.devcontainer/dev.env --name ticketing-hsg ticketing-hsg"
 alias test-run-stop="docker stop ticketing-hsg"
