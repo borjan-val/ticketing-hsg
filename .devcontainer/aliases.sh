3edbced14dea75105cdc9226dda6298ba4a7378a
Borjan Valchanov <borjan-val@proton.me>
2025-03-10 21:51:21 +0000

    Made development environment HTTPS-only by including nginx as a reverse proxy. Also renamed "db" container to "postgres" for consistency with "nginx" container name.

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -1,5 +1,5 @@
 alias test-build="docker build -t ticketing-hsg /workspaces/ticketing-hsg"
-alias test-run="docker run -d --rm -p 80:80 --add-host host.docker.internal=host-gateway --env-file /workspaces/ticketing-hsg/.devcontainer/dev.env --name ticketing-hsg ticketing-hsg"
-alias test-run-attached="docker run --rm -p 80:80 --add-host host.docker.internal=host-gateway --env-file /workspaces/ticketing-hsg/.devcontainer/dev.env --name ticketing-hsg ticketing-hsg"
+alias test-run="docker run -d --rm --network host --env-file /workspaces/ticketing-hsg/.devcontainer/dev.env --name ticketing-hsg ticketing-hsg"
+alias test-run-attached="docker run --rm --network host --env-file /workspaces/ticketing-hsg/.devcontainer/dev.env --name ticketing-hsg ticketing-hsg"
 alias test-run-stop="docker stop ticketing-hsg"
 alias test-update="test-run-stop; test-build; test-run"
